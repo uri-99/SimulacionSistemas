07cb4ee8152a6c9ef29ed5f7307ac7ae8e81f491
Joaquin Battilana <battilanajn@gmail.com>
2021-09-02 05:35:09 -0300

    agregado de grafico simple

---
+++ tp2/graphics.py
@@ -0,0 +1,92 @@
+import os
+from matplotlib import pyplot as plt
+import numpy as np
+
+def initialize_empty_matrix(size):
+    matrix = []
+    for _ in range(size):
+        matrix.append([])
+    return matrix
+
+def file_to_array(name, params_quantity):
+    dirname = os.path.dirname(__file__)
+    simulationFile = os.path.join(dirname, name)
+    data = open(simulationFile, 'r')
+    line = data.readline().rstrip('\n')
+    data_array = initialize_empty_matrix(params_quantity)
+    while(line != '' and line!= '\n'):
+        array_line = line.split(' ')
+        array_line.pop()
+        for j in range(params_quantity):
+            data_array[j].append(int(array_line[j]))
+        line = data.readline().rstrip('\n')
+    return data_array
+
+def filename(particles, size, iteration):
+    return 'data/data_'+str(particles)+'-'+str(size)+'_'+str(iteration)+'.txt'
+
+def promedy_max_min(iterations):
+    iterations_data = initialize_empty_matrix(3)
+    for i in range(iterations):
+        data = file_to_array(filename(1000, 51, i+1), 3)
+        for j in range(len(data)):
+            iterations_data[j].append(data[j])
+    return iterations_data
+
+def max_array_index(arrays):
+    max_array_index = 0
+    for i in range(1, len(arrays)):
+        if(len(arrays[i]) > max_array_index):
+            max_array_index = i
+    return max_array_index
+
+def average_arrays(arrays):
+    initial_array = np.copy(arrays[0])
+    for i in range(1, len(arrays)):
+        for j in range(len(arrays[i])):
+            if(len(initial_array) <= j):
+                np.append(initial_array, arrays[i][j])
+            else:
+                initial_array[j] = (initial_array[j] + arrays[i][j]) / 2
+    return initial_array
+
+def max_arrays(arrays):
+    initial_array = np.copy(arrays[0])
+    for i in range(1, len(arrays)):
+        for j in range(len(arrays[i])):
+            if(len(initial_array) <= j):
+                np.append(initial_array, arrays[i][j])
+            else:
+                if (initial_array[j] < arrays[i][j]):
+                    initial_array[j] = arrays[i][j]
+    return initial_array
+
+def min_arrays(arrays):
+    initial_array = arrays[0]
+    for i in range(1, len(arrays)):
+        for j in range(len(arrays[i])):
+            if(len(initial_array) <= j):
+                np.append(initial_array, arrays[i][j])
+            else:
+                if (initial_array[j] > arrays[i][j]):
+                    initial_array[j] = arrays[i][j]
+    return initial_array
+
+frame_array, left_particles_array, right_particles_array = promedy_max_min(5)
+
+average_frame = average_arrays(frame_array)
+average_left_particles = average_arrays(left_particles_array)
+average_right_particles = average_arrays(right_particles_array)
+max_left_particles = max_arrays(left_particles_array)
+min_left_particles = min_arrays(left_particles_array)
+max_right_particles = max_arrays(right_particles_array)
+min_right_particles = min_arrays(right_particles_array)
+
+plt.errorbar(average_frame, average_left_particles, ecolor="grey", label="lado izquierdo", fmt='o', ms=2, yerr=[np.subtract(average_left_particles, min_left_particles), np.subtract(max_left_particles, average_left_particles)])
+plt.errorbar(average_frame, average_right_particles, ecolor='grey', label="lado derecho", fmt='o', ms=2,  yerr=[np.subtract(average_right_particles, min_right_particles), np.subtract(max_right_particles, average_right_particles)])
+
+plt.xlabel('iteraciones')
+plt.ylabel('partículas')
+
+plt.legend()
+plt.show()
--- tp2/simulation.py
+++ tp2/simulation.py
@@ -6,15 +6,15 @@ from matplotlib.animation import FuncAnimation, PillowWriter
 matplotlib.use('QT5Agg')
 
 offset_pair = 0.5
-offset_odd = 1.5
+offset_odd = 0.5
 
 grid = Grid(50,25)
 fig = plt.gcf()
 
 def calculate_position(matrix_index):
     if(matrix_index % 2 == 0):
-        return matrix_index+offset_pair
-    return matrix_index+offset_odd
+        return matrix_index*2 + 1.5
+    return matrix_index*2
 
 def grid_to_particle(grid):
     particles_x = []
@@ -23,8 +23,8 @@ def grid_to_particle(grid):
         for j in range(203):
             for vector in grid.cellArray[i][j].newVectors:
                 if(vector == True):
-                    particles_y.append(i+1)
-                    particles_x.append(calculate_position(j))
+                    particles_x.append(i*2)
+                    particles_y.append(calculate_position(j))
     return particles_x, particles_y
 
 def animate(frame_number):
@@ -38,8 +38,9 @@ def animate(frame_number):
 
 particles_x, particles_y = grid_to_particle(grid)
 d,  = plt.plot(particles_x, particles_y, 'go', markersize=1)
-anim = animation.FuncAnimation(fig, animate, frames=200, interval=45, repeat=True)
+anim = animation.FuncAnimation(fig, animate, frames=200, interval=5000, repeat=True)
 #anim.save("movie.gif", writer=PillowWriter(fps=5))
-plt.xlim([0, 203])
-plt.ylim([0, 203])
+plt.xlim([0, 406])
+plt.ylim([0, 406])
+plt.gca().set_aspect('equal', adjustable='box')
 plt.show()
