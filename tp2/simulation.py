4e5202b34309e33a63acda6917bf7d649c26d864
Uriel <umihura@itba.edu.ar>
2021-09-13 10:14:44 -0300

    cosa

--- tp2/simulation.py
+++ tp2/simulation.py
@@ -6,7 +6,6 @@ from matplotlib.animation import FuncAnimation, PillowWriter
 import numpy as np;
 import sys
 import math
-matplotlib.use('QT5Agg')
 
 offset_pair = 0.5
 offset_odd = 0.5
@@ -20,10 +19,10 @@ matrix = np.load('./simulation/data_'+str(particles)+'-'+str(opening)+'_'+str(it
 
 
 def calculate_position(i, j):
-    i_offset = i
-    j_offset = j
-    if(j % 2 == 0):
-       j_offset += 0
+    i_offset = i #y
+    j_offset = j #x
+    if(i % 2 == 0):
+       j_offset -= 0.5 #0.5
     return i_offset, j_offset
 
 def matrix_to_particles(matrix):
@@ -67,16 +66,20 @@ def animate(frame_number):
     d, = plt.plot(new_particles_x_left, new_particles_y_left, 'bo', color=(0, 1, (len(new_particles_x_right)*2/particles)), markersize=2)
     d1, = plt.plot(new_particles_x_right, new_particles_y_right, 'bo', color=(0, (len(new_particles_x_right)*2/particles), 1), markersize=2)
     #d, = plt.plot(new_particles_x, new_particles_y, 'go', markersize=2)
-    plt.plot([203, 0], [203, 203])
+    
 
 particles_x, particles_y = matrix_to_particles(matrix[0])
 new_particles_x_left, new_particles_x_right, new_particles_y_left, new_particles_y_right = separate_particles(particles_x, particles_y, particles_x)
 d, = plt.plot(new_particles_x_left, new_particles_y_left, 'bo', color=(0, 1, 0), markersize=2)
 d1, = plt.plot(new_particles_x_right, new_particles_y_right, 'bo', color=(0, 0, 1), markersize=2)
 draw_mid_line(opening)
-anim = animation.FuncAnimation(fig, animate, frames=len(matrix)-1, interval=30, repeat=False)
-# anim.save("./simulation_videos/movie.avi", writer=PillowWriter(fps=30))
+plt.plot([0, 203], [0, 0], color="grey") #abajo
+plt.plot([0, 0], [203, 0], color="grey") #izq
+plt.plot([0, 203], [203, 203], color="grey") #arriba
+plt.plot([203, 203], [0, 203], color="grey") #derecha
+anim = animation.FuncAnimation(fig, animate, frames=1000, interval=1, repeat=False)
+anim.save("./simulation_videos/2000_25.gif", writer=PillowWriter(fps=30))
 plt.xlim([0, 203])
 plt.ylim([0, 203])
 plt.gca().set_aspect('equal', adjustable='box')
-plt.show()
+#plt.show()
