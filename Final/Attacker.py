2514567c42a8c45b69ab2f39b983ada4a754f881
Uriel <umihura@itba.edu.ar>
2021-12-08 12:38:48 -0300

    Update Attacker.py

--- Final/Attacker.py
+++ Final/Attacker.py
@@ -15,16 +15,28 @@ class Attacker:
         self.size = size
         self.mass = mass
         self.isDead = False
+        self.kn = 1.2 * 10 **5
+        self.kt = 2.4 * 10**5
 
     def move(self, dt, VIP, guards, attackers):
-        granular = 0
-        social = 0
-        drive = 0
+        granular = [0, 0]
+        social = [0, 0]
+        drive = [0, 0]
 
         for person in guards + attackers:
             dist = distanceBetween(self, person)
             if dist <= (self.size/2) + (person.size/2):
-                granular += #todo formula falopa de granular
+                dif = dist - (self.size + person.size)
+                difx = self.x - person.x
+                dify = self.y - person.y
+                angle = math.atan(dify / difx) #between 2 wachines, osea angulo normal
+                granular[0] += -dif * self.kn *math.cos(-angle) #todo formula falopa de granular
+                granular[1] += -dif * self.kn *math.sin(-angle)
+
+                angle += math.pi/2 #ahora es tangencial? podría ser -=pi/2
+                granular[0] += vt * dif * self.kt * cos(-angle)#vt velocidad relativa tangencial?
+                granular[1] += vt * dif * self.kt * sin(-angle)
+
             social += #todo formula falopa social
             drive =
 
